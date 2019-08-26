fc1c344cc95137893e0e7cfcec1b3f9f82b04eef
ansunon <ansunon221@gmail.com>
2019-08-26 11:51:57 +0900

    Add Customer API Loading View

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import TableHead from '@material-ui/core/TableHead';
 import TableBody from '@material-ui/core/TableBody';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import { withStyles } from '@material-ui/core/styles';
 
 const styles = theme => ({
@@ -17,16 +18,25 @@ const styles = theme => ({
   },
   table: {
     minWidth: 1080
+  },
+  progress: {
+    margin: theme.spacing.unit * 2
   }
 });
 
 class App extends React.Component {
 
   state = {
-    customers: ""
+    customers: "",
+    completed: 0 // 프로그래스 바를 위한 변수
+  }
+  
+  componentWillUnmount() {
+    clearInterval(this.timer);
   }
   
   componentDidMount() {
+    this.timer = setInterval(this.progress, 20); // 0.02초 마다 움직이게 한다.
     this.callApi()
     .then(res => this.setState({customers: res}))
     .catch(err => console.log(err));
@@ -38,13 +48,18 @@ class App extends React.Component {
     return body;
   }
 
+  progress = () => {
+    const { completed } = this.state;
+    this.setState({ completed: completed >= 100 ? 0 : completed + 1 });
+  }
+
   render() {
     const { classes } = this.props;
     return (
       <Paper className={classes.root}>
         <Table className={classes.table}>
           <TableHead>
-            <TableCell>번호</TableCell>
+            <TableCell>번호</TableCell> 
             <TableCell>이미지</TableCell>
             <TableCell>이름</TableCell>
             <TableCell>생년월일</TableCell>
@@ -55,8 +70,13 @@ class App extends React.Component {
           { 
             this.state.customers ? this.state.customers.map(c => {
             return ( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/> );
-          })
-        : "" }
+          }) :
+          <TableRow>
+            <TableCell colSpan='6' align="center">
+              <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed}/>
+            </TableCell>
+          </TableRow>
+        }
         </TableBody>
       </Table>
       </Paper>
