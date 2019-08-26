a56eff27f65beb8e82ae0eec8f4a760bb89e9f55
ansunon <ansunon221@gmail.com>
2019-08-26 14:34:14 +0900

    Create Customer datbase Table & Node.js Database Connection Settings

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs'); // 파일에 접근하기 위한 변수
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -6,33 +7,25 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data); //  환경 변수를 파싱
+const mysql = require('mysql'); // mysql 라이브러리를 불러와서 mysql 변수에 넣는다.
+
+const connection = mysql.createConnection({
+  host: conf.host,
+  user: conf.user,
+  password: conf.password,
+  port: conf.port,
+  database: conf.database
+})
+
 app.get('/api/customers', (req, res) => {
-    res.send([
-    {
-      'id': '1',
-      'image': 'https://placeimg.com/64/64/any',
-      'name': '홍길동',
-      'birthday': '999999',
-      'gender': '여자',
-      'job': '대학원생'
-    },
-    {
-      'id': '2',
-      'image': 'https://placeimg.com/64/64/1',
-      'name': '김철수',
-      'birthday': '12345',
-      'gender': '남자',
-      'job': '대학원'
-    },
-    {
-      'id': '3',
-      'image': 'https://placeimg.com/64/64/2',
-      'name': '김영희',
-      'birthday': '123456',
-      'gender': '여자',
-      'job': '대학원생'
+  connection.query(
+    "SELECT * FROM CUSTOMER", 
+    (err, rows, fields) => {
+        res.send(rows);
     }
-    ])
+  )
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
